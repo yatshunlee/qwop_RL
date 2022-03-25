8e12ea1886a04e28cd6a7fdf7469ccb35f709bc2
Jasper Lee <69416199+yatshunlee@users.noreply.github.com>
2022-03-26 02:16:41 +0800

    updated game_env.py and train.py test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+from game_env import qwopEnv
+from stable_baselines3 import DQN
+
+if __name__ == '__main__':
+    env = qwopEnv()
+
+    model = DQN.load("qwop")
+
+    obs = env.reset()
+    while True:
+        if env.gameover:
+            obs = env.reset()
+        else:
+            action, _states = model.predict(obs, deterministic=True)
+            # return obs, reward, done, info from step function
+            env.step(action)
